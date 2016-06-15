615b923a3e07b5a4e33fa0763c7d44d655055690
GhoastLee <17773454017@qq.com>
2016-06-15 22:48:43 +0800

    succeed

--- drag.js
+++ drag.js
@@ -1,8 +1,22 @@
+window.onload=function(){
+	drag('div1');
+}
 function drag(id){
-	var oDiv=document.getElementById('div1');
+	var oDiv=document.getElementById(id);
 	oDiv.onmousedown=function(ev){
 		var ev=ev||window.event;
 		var disX=ev.clientX-oDiv.offsetLeft;
 		var disY=ev.clientY-oDiv.offsetTop;
+		document.onmousemove=function(ev){
+			var ev=ev||window.event;
+			oDiv.style.left=ev.clientX-disX+'px';
+			oDiv.style.top=ev.clientY-disY+'px';
+		}
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
+		}
+		ev.preventDefault();
 	}
 }
+//drag('div1');
