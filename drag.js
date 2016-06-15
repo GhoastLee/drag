260f58d00a6bf5c68fb7b334414a255f0a9733f2
GhoastLee <17773454017@qq.com>
2016-06-15 21:31:37 +0800

    2016.6

--- drag.js
+++ drag.js
@@ -1,3 +1,8 @@
-function drag(){
-	
+function drag(id){
+	var oDiv=document.getElementById('div1');
+	oDiv.onmousedown=function(ev){
+		var ev=ev||window.event;
+		var disX=ev.clientX-oDiv.offsetLeft;
+		var disY=ev.clientY-oDiv.offsetTop;
+	}
 }
